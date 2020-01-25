d3999c2f80593d63bbb1ec1b19d2fab0e8bd1752
Alex Chebotarsky <goodleby@gmail.com>
2020-01-26 01:28:55 +0200

    Developing commit, full code rewriting, clean point

--- demo/main.js
+++ demo/main.js
@@ -1,10 +1,9 @@
 $('.myInput1').slidinput();
 
 $('.myInput2').slidinput({
-  mode: 'centered'
+
 });
 
 $('.myInput3').slidinput({
-  mode: 'centered',
-  scaling: 0.7
+  
 });
--- dist/slidinput.js
+++ dist/slidinput.js
@@ -1,92 +1,64 @@
 'use strict';
 (function($) {
-  const addUpPx = (...values) => values.reduce((acc, item) => acc + pxToNum(item), 0);
-  const compareStyle = (style, elem1, elem2) =>
-    $(elem1).css(style) !== $(elem2).css(style) ? $(elem1).css(style) : '';
-  const pxToNum = value => Number(value.slice(0, -2));
-  const label = $(document.createElement('label')).addClass('slidinput-wrapper');
-  const span = $(document.createElement('span')).addClass('slidinput-placeholder');
-  const defs = {
-    scaling: 0.5,
-    sliding: 4.5,
-    placeholderPadding: 0,
-    mode: false
-  };
+  const getClassesString = (classList, postfix = '') =>
+    Array.from(classList)
+      .map(item => item + postfix)
+      .join(' ');
+  const pxToNum = (...values) =>
+    values.reduce((acc, item) => acc + Number(item.slice(0, -2)), 0);
+  const compareStyle = (style, target1, target2) =>
+    $(target1).css(style) !== $(target2).css(style) ? $(target1).css(style) : '';
+  const defs = {};
   $.fn.slidinput = function(options) {
     options = $.extend(defs, options);
     return this.each(function() {
       $(this).wrap(
-        label.clone().addClass(
-          Array.from(this.classList)
-            .map(item => item + '-wrapper')
-            .join(' ')
+        $(document.createElement('label')).addClass(
+          getClassesString(['slidinput', ...this.classList], '-wrapper')
         )
       );
-      const wrapper = $(this)
-        .parent()
-        .get()[0];
-      const placeholder = span.clone().get()[0];
-      $(wrapper).append(placeholder);
-      $(wrapper).css({
-        marginLeft: compareStyle('marginLeft', this, wrapper),
-        marginTop: compareStyle('marginTop', this, wrapper),
-        marginRight: compareStyle('marginRight', this, wrapper),
-        marginBottom: compareStyle('marginBottom', this, wrapper)
+      const $wrapper = $(this).parent();
+      const $placeholder = $(document.createElement('span'))
+        .addClass('slidinput-placeholder')
+        .text($(this).attr('placeholder'));
+      $wrapper.append($placeholder).css({
+        marginLeft: compareStyle('marginLeft', this, $wrapper),
+        marginTop: compareStyle('marginTop', this, $wrapper),
+        marginRight: compareStyle('marginRight', this, $wrapper),
+        marginBottom: compareStyle('marginBottom', this, $wrapper)
       });
-      $(placeholder)
-        .text($(this).attr('placeholder'))
-        .css({
-          color: compareStyle('color', this, placeholder),
-          fontSize: compareStyle('fontSize', this, placeholder),
-          fontFamily: compareStyle('fontFamily', this, placeholder),
-          letterSpacing: compareStyle('letterSpacing', this, placeholder),
-          fontWeight: compareStyle('fontWeight', this, placeholder),
-          top: addUpPx($(this).css('paddingTop'), $(this).css('borderTopWidth')),
-          left: addUpPx($(this).css('paddingLeft'), $(this).css('borderLeftWidth'))
-        });
       $(this)
         .removeAttr('placeholder')
         .css({
           margin: 0
         })
-        .on('focus', () => {
-          $(wrapper).addClass('focused');
-        })
-        .on('blur', () => {
-          $(wrapper).removeClass('focused');
-        })
+        .on('focus', () => $wrapper.addClass('focused'))
+        .on('blur', () => $wrapper.removeClass('focused'))
         .on('input', () => {
-          if (
-            $(this)
-              .val()
-              .trim()
-          ) {
-            $(wrapper).addClass('filled');
-          } else {
-            $(wrapper).removeClass('filled');
-          }
+          $(this)
+            .val()
+            .trim()
+            ? $wrapper.addClass('filled')
+            : $wrapper.removeClass('filled');
         });
-      options.scaling && placeholder.style.setProperty('--scaling', options.scaling);
-      switch (options.mode) {
-        case 'centered':
-          let newPadding =
-            ($(this).outerHeight() -
-              addUpPx($(this).css('borderTopWidth'), $(this).css('borderBottomWidth')) -
-              $(placeholder).height() * options.scaling -
-              options.placeholderPadding -
-              $(this).height()) /
-            2;
-          let oldPadding = pxToNum($(this).css('paddingTop'));
-          let paddingDiff = oldPadding - newPadding;
-          placeholder.style.setProperty('--sliding', paddingDiff / options.scaling + 'px');
-          $(this).css({
-            paddingBottom: newPadding,
-            paddingTop: oldPadding + paddingDiff
-          });
-          break;
-        case 'above':
-          break;
-      }
+      $placeholder.css({
+        color: compareStyle('color', this, $placeholder),
+        fontSize: compareStyle('fontSize', this, $placeholder),
+        fontFamily: compareStyle('fontFamily', this, $placeholder),
+        letterSpacing: compareStyle('letterSpacing', this, $placeholder),
+        fontWeight: compareStyle('fontWeight', this, $placeholder),
+        top: 0,
+        left: 0
+      });
+      let placeholderTop = ($wrapper.outerHeight() - $placeholder.height()) / 2;
+      let placeholderLeft = pxToNum(
+        $(this).css('borderLeftWidth'),
+        $(this).css('paddingLeft')
+      );
+      $placeholder.css({
+        top: placeholderTop,
+        left: placeholderLeft
+      });
     });
   };
 })(jQuery);
